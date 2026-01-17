809e898eec2ba16ebc803382a82e36467bd4eb30
Pabl0t0rr <Pablotorr04@gmail.com>
2026-01-17 16:55:42 +0100

    Fix multiple creation per ssesion and include reserve on user

--- src/controllers/reservation.controllers.ts
+++ src/controllers/reservation.controllers.ts
@@ -1,11 +1,15 @@
 import { getDB } from "../db/mongo";
 import { ObjectId } from "mongodb";
 import { Reservation, ReservationStatus } from "../types/reservation";
-import { reservationCollection } from "../utils/utils";
+import { reservationCollection, userCollection } from "../utils/utils";
 import { sessionCollection } from "../utils/utils";
 
 export const createReservation = async (userId: string, sessionId: string, date: string) => {
     const db = getDB(); 
+
+    //To check if user has a reservation done for the same session
+    await checkExistingReservation (userId, sessionId);
+
     const newReservation = await db.collection<Reservation>(reservationCollection).insertOne({
         _id : new ObjectId(),
         userId,
@@ -14,6 +18,9 @@ export const createReservation = async (userId: string, sessionId: string, date:
         status: ReservationStatus.CONFIRMED
     });
     
+    //Add the reservation to the user
+    await addReservationUser(userId, newReservation.insertedId.toString());
+
     const reservationCreated = await db.collection<Reservation>(reservationCollection).findOne({_id: newReservation.insertedId});
 
     return reservationCreated;
@@ -31,8 +38,22 @@ export const changeStatusReservation = async (userId : string ,sessionId: string
     return changeReservation;
 }
 
+const checkExistingReservation = async (userId: string, sessionId: string) => {
+    const db = getDB();
+    const existingReservation = await db.collection<Reservation>(reservationCollection).findOne({userId: userId, session: sessionId});
+    if(existingReservation) throw new Error("You have already reserved this session");
 
+    return existingReservation;
+}
 
+const addReservationUser = async (userId: string, reservationId: string) => {
+    const db = getDB();
+    const addedReservation = await db.collection(userCollection).updateOne(
+        {_id : new ObjectId(userId)},
+        {$push : {reservations : new ObjectId(reservationId)} as any});
+        
+    return addedReservation;
+}
 // Field Resolvers
 
 export const getReservationSession = async (parent : Reservation) => {
